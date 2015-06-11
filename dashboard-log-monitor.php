97a33ad09099642203b89093565eb76efb62dab8
Onni Hakala <onni@seravo.fi>
2015-06-11 22:22:16 +0300

    bump up version

--- dashboard-log-monitor.php
+++ dashboard-log-monitor.php
@@ -5,7 +5,7 @@
  * Description: Take a sneak peek on your access logs from the wordpress dashboard.
  * Author: Onni Hakala / Seravo Oy
  * Author URI: http://seravo.fi
- * Version: 1.0.3
+ * Version: 1.0.4
  * License: GPLv2 or later
 */
 /** Copyright 2014 Seravo Oy
